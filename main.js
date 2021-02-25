fd5e095becf9a77c6d8a3f71d1c547e7fed62da1
taranakajima <nakatarat@gmail.com>
2021-02-25 09:19:55 -0800

    1. exercise

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 const app = Vue.createApp({
     data () {
         return {
-            product: 'Socks'
+            product: 'Socks',
+            description: 'A lovely pair of socks'
         }
     }
 })
