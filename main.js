8721233abeb5aa8e3fd6ccb25d8358f114022a49
taranakajima <nakatarat@gmail.com>
2021-02-25 09:42:14 -0800

    Finished L4

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ const app = Vue.createApp({
     data() {
         return {
             product: 'Socks',
+            onSale: true,
             image: './assets/images/socks_blue.jpg'
         }
     }
