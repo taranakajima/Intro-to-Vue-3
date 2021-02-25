3869469bc17f1280d9470f1c93842d096f0e4355
taranakajima <nakatarat@gmail.com>
2021-02-25 09:56:20 -0800

    Finish L5

--- main.js
+++ main.js
@@ -4,7 +4,18 @@ const app = Vue.createApp({
             product: 'Socks',
             image: './assets/images/socks_blue.jpg',
             inStock: true,
-            details: ['50% cotton', '30% wool', '20% polyester']
+            details: ['50% cotton', '30% wool', '20% polyester'],
+            variants: [
+                { id: 2234, color: 'green' },
+                { id: 2235, color: 'blue' }
+            ],
+            sizes: [
+                { id: 'size-XS', size: 'XS' },
+                { id: 'size-S', size: 'S' },
+                { id: 'size-M', size: 'M'},
+                { id: 'size-L', size: 'L'},
+                { id: 'size-XL', size: 'XL' }
+            ]
         }
     }
 })
