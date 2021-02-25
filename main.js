50a7fe97c0ea780c8bc9f4ee4c71f5b158f5a421
taranakajima <nakatarat@gmail.com>
2021-02-25 09:29:43 -0800

    Finish L3 exercise

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 const app = Vue.createApp({
     data() {
         return {
-            product: 'Socks'
+            product: 'Socks',
+            image: './assets/images/socks_blue.jpg',
+            url: 'https://www.google.com/search?q=vue+socks&sxsrf=ALeKk011JRQibcfNpqjHOUgNNBBaFpnJtg:1614274051157&source=lnms&tbm=isch&sa=X&ved=2ahUKEwiOn467x4XvAhVJsZ4KHb-eCXYQ_AUoAnoECAQQBA&biw=1440&bih=789#imgrc=tRXYwf5E6vJ4YM'
         }
     }
 })
